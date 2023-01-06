2918e7cd309e46803d1f6ed5862f4040861b8619
ImtiazUddin28 <imtiaz100463@gmail.com>
2023-01-06 12:34:37 +0600

    files basic ready

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,24 +1,51 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
 
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
 
-    const handleLogin = event =>{
+    const from = location.state?.from?.pathname || '/';
+
+    const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
 
         login(email, password)
-        .then( result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .then(error => console.log(error));
+            .then(result => {
+                const user = result.user;
+
+
+                const currentUser = {
+                    email: user.email
+                }
+
+                console.log(currentUser);
+
+                // get jwt token
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        // local storage is the easiest but not the best place to store jwt token
+                        localStorage.setItem('car-token', data.token);
+                        navigate(from, { replace: true });
+                    });
+                
+            })
+            .catch(error => console.log(error));
     }
 
     return (
@@ -40,7 +67,7 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <Link to="/signup" className="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
@@ -49,7 +76,7 @@ const Login = () => {
                             <input className="btn btn-primary" type="submit" value="Login" />
                         </div>
                     </form>
-                    <p className='text-center'>New to Genius Car <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                    <p className='text-center'>New to car Car <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
                 </div>
             </div>
         </div>
