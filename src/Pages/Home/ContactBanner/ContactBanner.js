2c335ef5010e968f17d66f75fbb1f385d73bb519
ImtiazUddin28 <imtiaz100463@gmail.com>
2023-01-07 16:54:06 +0600

    contact banner section is done

--- src/Pages/Home/ContactBanner/ContactBanner.js
+++ src/Pages/Home/ContactBanner/ContactBanner.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+import { ImHome, ImPhone,ImLocation } from "react-icons/im";
 const ContactBanner = () => {
     return (
-        <div>
-            <div className="p-6 py-12 dark:bg-violet-400 dark:text-gray-900">
-	<div className="container mx-auto">
-		<div className="flex flex-col lg:flex-row items-center justify-between">
-			<h2 className="text-center text-6xl tracking-tighter font-bold">Up to
-				<br className="sm:hidden"/>50% Off
-			</h2>
-			<div className="space-x-2 text-center py-2 lg:py-0">
-				<span>Plus free shipping! Use code:</span>
-				<span className="font-bold text-lg">MAMBA</span>
+        
+    <div className="p-12 py-16 my-24 bg-black text-white rounded-xl">
+		<div className="container mx-auto">
+		<div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<div className="flex gap-3">
+				<div className='text-4xl py-2 mx-3'><ImHome></ImHome></div>
+				<div>
+				<h1>We are open at Monday to Friday</h1>
+				<h1 className='text-xl font-bold'>8.00 am - 10.00 pm </h1>
+				</div>
+
 			</div>
-			<Link href="#" rel="noreferrer noopener" className="px-5 mt-4 lg:mt-0 py-3 rounded-md border block dark:bg-gray-50 dark:text-gray-900 dark:border-gray-400">Shop Now</Link>
+			<div className="flex gap-3">
+				<div className='text-4xl py-2 mx-3'><ImPhone></ImPhone></div>
+				<div>
+				<h1>Have a Question?</h1>
+				<h1 className='text-xl font-bold'>+8801840930768</h1>
+				</div>
+			</div>
+			<div className="flex gap-3 ">
+				<div className='text-4xl py-2 mx-3'><ImLocation></ImLocation></div>
+				<div>
+				<h1>Need Repair? Our Address</h1>
+				<h1 className='text-xl font-bold'>Mirsarai Street, Chittagong</h1>
+				</div>
+
+			</div>
+		</div>
 		</div>
 	</div>
-</div>
-        </div>
+        
     );
 };
 
